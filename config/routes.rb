6d6af6c04e65f07343beb0f305b8d064fce30178
izunarin <112565053+izunarin@users.noreply.github.com>
2022-11-26 11:44:59 +0000

    routes,application.html.erbいったん終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+ namespace :admin do
+  resources :order_details, only: [:update] 
+  resources :orders, only: [:orders, :update, :show] 
+  resources :customers, only: [:index, :show, :edit, :update]
+  resources :genres, only: [:index, :create, :edit, :update] 
+  resources :items, only: [:index, :new, :create, :show, :edit, :update] 
+  resources :homes, only: [:top] 
+  end
+ 
+ namespace :public do
+  resources :addresses, only: [:index, :edit, :create, :update, :destroy] 
+  resources :cart_items, only: [:index, :update, :destroy, :create]
+  resources :orders, only: [:new, :create, :index, :show]
+  resource :customers,only:[:show, :edit, :update]
+  resources :items, only: [:index, :show]
+  resources :homes, only: [:top, :about]
+  end
+  
+  namespace :public do
+    get 'orders/unsubscribe' => "orders#confirm"
+    get 'orders/withdraw' => "orders#thanks"
+  end
+ 
+ namespace :public do
+    get 'cart_items/destroy_all' => "cart_items#destroy_all"
+  end
+ 
+  namespace :public do
+    get 'customers/unsubscribe' => "customers#unsubscribe"
+    get 'customers/withdraw' => "customers#withdraw"
+  end
+
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+  }
+  
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+  }
+
 end
