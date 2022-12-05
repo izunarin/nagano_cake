09a6451d2e2958215a0c07ba89762ca8de06407f
izunarin <112565053+izunarin@users.noreply.github.com>
2022-12-05 13:15:59 +0000

    会員編集（確認後回し）

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -4,11 +4,23 @@ class Admin::CustomersController < ApplicationController
   end
 
   def show
+     @customer = Customer.find(params[:id])
+     @order = @customer.order
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    customer = Customer.find(params[:id])
+    customer.update(customer_params)
+    redirect_to admin_customer_path
+  end
+  
+   private
+  
+  def customer_params
+    params.require(:list).permit(:last_name, :first_name, :ast_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 end
