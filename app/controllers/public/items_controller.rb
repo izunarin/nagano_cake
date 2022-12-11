7390f4356b26be894b0b6047102e11849599bc6b
izunarin <112565053+izunarin@users.noreply.github.com>
2022-12-11 15:40:48 +0000

    色々中途半端

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,17 @@
 class Public::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @genres = Genre.all
+    if params[:genre_id]
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items
+    else
+      @items = Item.all
+    end
+    
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 end
